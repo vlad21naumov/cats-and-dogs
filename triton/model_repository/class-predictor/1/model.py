12ba3a710778e70ab168cbf49e1555a6b1ad8800
naumov-vladis <naumov-vladis@yandex.team>
2025-04-29 00:57:09 +0300

    fix

---
+++ triton/model_repository/class-predictor/1/model.py
@@ -0,0 +1,31 @@
+import numpy as np
+import triton_python_backend_utils as pb_utils
+
+OUTPUT_DTYPE = np.int32
+
+
+class TritonPythonModel:
+    def initialize(self, args):
+        self.output_dtype = OUTPUT_DTYPE
+
+    def _get_classes(self, logits):
+        preds = np.argmax(logits, axis=1)
+        return preds.astype(self.output_dtype)
+
+    def execute(self, requests):
+        responses = []
+        for request in requests:
+            logits_tensor = pb_utils.get_input_tensor_by_name(request, "LOGITS")
+            logits = logits_tensor.as_numpy()
+
+            preds = self._get_classes(logits)
+            preds = np.expand_dims(preds, axis=1)
+
+            inference_response = pb_utils.InferenceResponse(
+                output_tensors=[
+                    pb_utils.Tensor("PREDICTED_CLASS", preds),
+                ]
+            )
+            responses.append(inference_response)
+
+        return responses
